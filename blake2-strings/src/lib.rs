1c1365f3e21b1061ce02bfdf8bd689bad3f38bc4
Zentheon <sushileaf@protonmail.com>
2026-04-24 10:02:38 -0500

    feat: add `blake2` and `blake3`

---
+++ blake2-strings/src/lib.rs
@@ -0,0 +1,35 @@
+use blake2::{Blake2b512, Blake2s256, Digest, digest::generic_array::GenericArray};
+use digest::consts::{U32, U64};
+use hash_strings_derive::{StringDigest, StringWrapper};
+
+#[derive(Clone, Debug, Eq, StringDigest, StringWrapper)]
+#[hash_string(hasher = Blake2s256, con = U32, digest = Digest, no_io_wrapper)]
+pub struct Blake2s256String(pub fstr::FStr<64>);
+
+#[derive(Clone, Debug, Eq, StringDigest, StringWrapper)]
+#[hash_string(hasher = Blake2b512, con = U64, digest = Digest, no_io_wrapper)]
+pub struct Blake2b512String(pub fstr::FStr<128>);
+
+impl From<&GenericArray<u8, U32>> for Blake2s256String {
+    fn from(digest: &GenericArray<u8, U32>) -> Self {
+        Self(base16ct::lower::encode_string(digest).try_into().unwrap())
+    }
+}
+
+impl From<GenericArray<u8, U32>> for Blake2s256String {
+    fn from(digest: GenericArray<u8, U32>) -> Self {
+        Self::from(&digest)
+    }
+}
+
+impl From<&GenericArray<u8, U64>> for Blake2b512String {
+    fn from(digest: &GenericArray<u8, U64>) -> Self {
+        Self(base16ct::lower::encode_string(digest).try_into().unwrap())
+    }
+}
+
+impl From<GenericArray<u8, U64>> for Blake2b512String {
+    fn from(digest: GenericArray<u8, U64>) -> Self {
+        Self::from(&digest)
+    }
+}
---
+++ blake3-strings/src/lib.rs
@@ -0,0 +1,64 @@
+use blake3::{Hash, Hasher};
+use digest::consts::U16;
+use fstr::FStr;
+use hash_strings_derive::StringWrapper;
+
+#[derive(Clone, Debug, Eq, StringWrapper)]
+#[hash_string(hasher = Hash, con = U16, hash_name = "Blake3", no_io_wrapper)]
+pub struct Blake3String(pub fstr::FStr<32>);
+
+impl Default for Blake3String {
+    fn default() -> Self {
+        Self::digest([])
+    }
+}
+impl From<Hash> for Blake3String {
+    fn from(value: Hash) -> Self {
+        Self(FStr::try_from(value.as_bytes()).unwrap())
+    }
+}
+
+impl Blake3String {
+    /// Digest some raw data and produce a hash.
+    pub fn digest(data: impl AsRef<[u8]>) -> Self {
+        let mut hasher = Hasher::new();
+        hasher.update(data.as_ref());
+        let hash = Self::from(hasher.finalize());
+
+        #[cfg(feature = "tracing")]
+        tracing::trace!(%hash, "Generated a Blake3 hash of raw data");
+
+        hash
+    }
+    /// Attempts to digest the entirety of the given reader.
+    ///
+    /// Returns errors produced by [`std::io::copy`]
+    pub fn digest_reader<R>(read: &mut R) -> Result<Self, std::io::Error>
+    where
+        R: Sized,
+        R: std::io::Read,
+    {
+        let mut hasher = Hasher::new();
+        let digested = std::io::copy(read, &mut hasher)?;
+        let hash = Self::from(hasher.finalize());
+
+        #[cfg(feature = "tracing")]
+        tracing::trace!(digested, %hash, "Generated a Blake3 hash of content in a reader");
+
+        Ok(hash)
+    }
+    /// Attempts to open the given path and digest the entirety of its bytes.
+    ///
+    /// Returns errors produced by [`std::fs::File::open`] and [`std::io::copy`]
+    pub fn digest_file(path: impl AsRef<std::path::Path>) -> Result<Self, std::io::Error> {
+        let mut file = std::fs::File::open(path.as_ref())?;
+        let mut hasher = Hasher::new();
+        let digested = std::io::copy(&mut file, &mut hasher)?;
+        let hash = Self::from(hasher.finalize());
+
+        #[cfg(feature = "tracing")]
+        tracing::trace!(digested, %hash, path = ?path.as_ref(), "Generated a Blake3 hash of a file");
+
+        Ok(hash)
+    }
+}
